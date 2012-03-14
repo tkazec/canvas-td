bf3d90412c9971ca18028e08dc08e78e363f8b94
Teddy Cross <tkazec@gmail.com>
2012-03-14 10:39:52 -0400

    Remove appcache and update build.sh

--- build.sh
+++ build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-# $ bash build.sh containerdir googleanalyticsacc
+# $ bash build.sh outdir ga-id
 
-TIME=`date +%s`
 SRCDIR="$( cd "$( dirname "$0" )" && pwd )"
-OUTDIR="$1/canvas-td"
+OUTDIR=$1
 GA=$2
 
 rm -rf $OUTDIR
@@ -13,11 +12,10 @@ cd $OUTDIR
 rm build.sh
 rm -rf .git
 
-#sed -i '' -e 's/<html/& manifest="manifest.appcache"/' index.html # disabled for now due to complications
 sed -i '' -e "s/##GA##/$GA/" index.html
 sed -i '' -e '/\.js"><\/script>/d' -e 's/<script src/<script src="index.js"><\/script>&/' index.html
-echo "#$TIME" >> manifest.appcache
 
 yuicompressor -o index.css index.css
 
 closure --language_in ECMASCRIPT5 --js helpers.js --js game.js --js maps/data.js --js turrets/data.js --js ui.js --js_output_file index.js
+rm helpers.js game.js maps/data.js turrets/data.js ui.js
