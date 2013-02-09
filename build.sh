d4578d9c6cebab96c86fa3a4ccf897ec9f8f8537
Teddy Cross <tkazec@gmail.com>
2013-02-09 14:21:48 -0800

    fix build script: closure -> closure-compiler

--- build.sh
+++ build.sh
@@ -17,5 +17,5 @@ sed -i '' -e '/\.js"><\/script>/d' -e 's/<script src/<script src="index.js"><\/s
 
 yuicompressor -o index.css index.css
 
-closure --language_in ECMASCRIPT5 --js js/helpers.js --js js/game.js --js js/maps.js --js js/turrets.js --js js/ui.js --js_output_file index.js
+closure-compiler --language_in ECMASCRIPT5 --js js/helpers.js --js js/game.js --js js/maps.js --js js/turrets.js --js js/ui.js --js_output_file index.js
 rm -r js
